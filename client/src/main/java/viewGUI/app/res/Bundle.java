a61413a06c5e5f7f1851f615379108cc3a1137cc
Lalayants <91465467+Lalayants@users.noreply.github.com>
2022-06-01 20:27:09 +0300

    English and Russian supported

---
+++ client/src/main/java/viewGUI/app/res/Bundle.java
@@ -0,0 +1,13 @@
+package viewGUI.app.res;
+
+import java.util.ListResourceBundle;
+
+public class Bundle extends ListResourceBundle {
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][] {
+                {"hello", "Hello"},
+                {"ourfeatures", new String[] {"Collaborative Translation", "Localization Workflow Management", "Localization Process Automation"}}
+        };
+    }
+}
